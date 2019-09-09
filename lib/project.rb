2c2956bd48103b9423f7ba1db9b00ee6448f0525
Christopher Dent <christopherdent01@gmail.com>
2019-09-09 22:45:04 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,9 +4,14 @@ class Project
   attr_reader :title
   attr_accessor :backers 
   
+  
+  
+@@all = []
+
   def initialize(title)
     @title = title 
     @backers = []
+    @@all = 
     
   end 
   
