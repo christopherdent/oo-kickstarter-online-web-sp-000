c6972fd67d0a95651814f62b3e7b024540e5a4e5
Christopher Dent <christopherdent01@gmail.com>
2019-09-09 22:12:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,26 +1,22 @@
 require 'pry'
 class Backer
 
- attr_accessor :project
+ t
 attr_reader :name 
   
   
   
   def initialize(name)
     @name=name 
-    @project = project 
-  end 
-  
-  
-  def backed_projects
+    @project = project
     @backed_projects = []
   end 
   
+
   
   def back_project(project)
     
-    @backed_projects << project   
-  
+   
     
   end 
   
