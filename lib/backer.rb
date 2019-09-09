fbcc67144790084d8dcdf459d1e6f31196bb0c46
Christopher Dent <christopherdent01@gmail.com>
2019-09-09 21:54:28 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Backer
+
+ 
+attr_reader :name 
+  
+  
+  def initialize(name)
+    @name=name 
+    
+  end 
+  
+  
+  def backed_projects
+    []
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
